92ec4ffe2de1bcbd06160e834165d76991497e22
termite9 <ttermite0@gmail.com>
2024-01-13 11:18:25 +0900

    중간

---
+++ hy.py
@@ -0,0 +1,63 @@
+Running cells with 'pythonProject1' requires the ipykernel package.
+
+Run the following command to install 'ipykernel' into the Python environment. 
+
+Command: 'c:/Users/고창섭/Desktop/python/pythonProject1/Scripts/python.exe -m pip install ipykernel -U --force-reinstall'
+
+
+
+
+Text(0.5, 1.0, 'matplotlib sample')
+
+---------------------------------------------------------------------------
+NameError                                 Traceback (most recent call last)
+Cell In[2], line 1
+----> 1 plt.plot(x,color='r')
+      2 plt.show()
+
+NameError: name 'plt' is not defined
+array([[0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+       [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]])
+array([[1., 1., 1., 1., 1., 1., 1., 1., 1., 1.],
+       [1., 1., 1., 1., 1., 1., 1., 1., 1., 1.]])
+
+
+
+b'<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<title>Quotes to Scrape</title>\n   '
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<meta charset="utf-8"/>
+<title>Quotes to Scrape</title>
+<link href="/static/bootstrap.min.css" rel="stylesheet"/>
+<link href="/static/main.css" rel="stylesheet"/>
+</head>
+<body>
+<div class="container">
+<div class="row header-box">
+<div class="col-md-8">
+<h1>
+<a href="/" style="text-decoration: none">Quotes to Scrape</a>
+</h1>
+</div>
+<div class="col-md-4">
+<p>
+<a href="/login">Login</a>
+</p>
+</div>
+</div>
+<div class="row">
+<div class="col-md-8">
+...
+</p>
+</div>
+</footer>
+</body>
+</html>
+Output is truncated. View as a scrollable element or open in a text editor. Adjust cell output settings...
+bs4.BeautifulSoup
+[<meta charset="utf-8"/>,
+ <title>Quotes to Scrape</title>,
+ <link href="/static/bootstrap.min.css" rel="stylesheet"/>,
+ <link href="/static/main.css" rel="stylesheet"/>]
